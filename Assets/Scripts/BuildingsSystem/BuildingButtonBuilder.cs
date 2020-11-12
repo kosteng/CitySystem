80ff57cb8c9c3638cf6e41bdbe84162fd1a40835
Константин Постукян <kpostukyan.craftgs@gmail.com>
2020-11-12 20:34:01 +0300

    add: заложил основы базы данных построек и билдер для кнопок покупки здания

---
+++ Assets/Scripts/BuildingsSystem/BuildingButtonBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingButtonBuilder 
+{
+   private readonly AllBuildingsDatabase _allBuildingsDatabase;
+
+   public BuildingButtonBuilder(AllBuildingsDatabase allBuildingsDatabase)
+   {
+      _allBuildingsDatabase = allBuildingsDatabase;
+   }
+
+   public void Create()
+   {
+      foreach (var button in _allBuildingsDatabase.BuildingDatabases)
+      {
+         // сделаю вид что я написал этот код
+      }
+   }
+}
--- Assets/Scripts/BuildingsSystem/Databases/AllBuildingsDatabase.cs
+++ Assets/Scripts/BuildingsSystem/Databases/AllBuildingsDatabase.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 
     [CreateAssetMenu(menuName ="DatabasesSO/AllBuildingsDatabase")]
     public class AllBuildingsDatabase : ScriptableObject
     {
         public HouseBuildingDatabase HouseBuildingDatabase;
+        [SerializeField] private List<ABuildingDatabase> _buildingDatabases;
+        public List<ABuildingDatabase> BuildingDatabases => _buildingDatabases;
     }
-
+[Serializable]
+public class ABuildingDatabase
+{
+  [SerializeField] private EBuildingType _buildingType;
+  [SerializeField] private string _name;
+  [SerializeField] private ABuildingView _view;
+}
