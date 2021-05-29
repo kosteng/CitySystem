028719b3d480a24d0c3caad2a3e03556572877f3
KPostukyan <kpostukyan.craftgs@gmail.com>
2021-05-29 14:19:09 +0300

    refactor: перенос логики из вьюхи в контроллер

--- Assets/Scripts/Characters/Controllers/CharacterMovementController.cs
+++ Assets/Scripts/Characters/Controllers/CharacterMovementController.cs
@@ -11,54 +11,60 @@ namespace Units.Controllers
     {
         private readonly CharacterView _characterView;
         private Camera _mainCamera;
-        private ECharacterState _characterState;
+        private ECharacterState _characterCurrentState;
+        private const int LEFT_MOUSE_BUTTON = 1;
+        private bool _isMoving;
+        private Vector3 _pointDestination;
+        private static readonly int Moving = Animator.StringToHash("Moving");
         public Transform UnitViewTransform => _characterView.transform;
 
         public CharacterMovementController(CharacterView characterView)
         {
             //todo нужна фабрика
             if (_characterView == null)
-                _characterView = MonoBehaviour.Instantiate(characterView);
+                _characterView = Object.Instantiate(characterView);
         }
 
-        public void StopUnit()
-        {
-            _characterView.NavMeshAgent.Stop(); 
-        }
-        
         public void Update(float deltaTime)
         {
-            PlayerMove(_mainCamera);
+            CheckTargetForMove(_mainCamera);
+            CheckStopState();
+            UpdateAnimation();
         }
 
-        private void PlayerMove(Camera camera)
+        private void CheckTargetForMove(Camera camera)
         {
-            if (Input.GetMouseButtonDown(1))
+            if (!Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON)) return;
+
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hit, 100f) && !EventSystem.current.IsPointerOverGameObject())
             {
-                var ray = camera.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(ray, out var hit, 100f) && !EventSystem.current.IsPointerOverGameObject())
-                {
-                    Debug.Log(hit.transform.gameObject.name);
-                    _characterView.MoveToPoint(hit.point);
-                }
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit, 100f))
-                {
- 
-                }
+                MoveToPoint(hit.point);
             }
         }
 
         public void Initialize()
         {
             _mainCamera = Camera.main;
+        }
+
+        private void MoveToPoint(Vector3 point)
+        {
+            _characterView.transform.LookAt(point);
+            _pointDestination = point;
+            _characterView.NavMeshAgent.SetDestination(_pointDestination);
+        }
+
+        private void CheckStopState()
+        {
+            _isMoving = _characterView.NavMeshAgent.remainingDistance > _characterView.NavMeshAgent.stoppingDistance;
+            _characterView.NavMeshAgent.isStopped = !_isMoving;
+        }
+        private void UpdateAnimation()
+        {
+
+            _characterView.Animator.SetBool(Moving, _isMoving);
         }
     }
 }
--- Assets/Scripts/Characters/Views/CharacterView.cs
+++ Assets/Scripts/Characters/Views/CharacterView.cs
@@ -9,24 +9,7 @@ namespace Units.Views
     {
         [SerializeField] private NavMeshAgent _agent;
         [SerializeField] private Animator _animator;
-
-        private bool _isMoving;
-        private Vector3 _pointDestination;
-        private static readonly int Moving = Animator.StringToHash("Moving");
         public NavMeshAgent NavMeshAgent => _agent;
-
-        public void MoveToPoint(Vector3 point)
-        {
-            transform.LookAt(point);
-            _pointDestination = point;
-            _agent.SetDestination(_pointDestination);
-        }
-
-        private void FixedUpdate()
-        {
-            _isMoving = _agent.remainingDistance > _agent.stoppingDistance;
-            _agent.isStopped = !_isMoving;
-            _animator.SetBool(Moving, _isMoving);
-        }
+        public Animator Animator => _animator;
     }
 }
