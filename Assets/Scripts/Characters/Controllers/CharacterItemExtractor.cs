2d6c3760dbc750f15cb66a43d2ea93624ff9a416
KPostukyan <kpostukyan.craftgs@gmail.com>
2021-06-10 19:59:55 +0300

    add: начал работу над извлекателями

---
+++ Assets/Scripts/Characters/Controllers/CharacterItemExtractor.cs
@@ -0,0 +1,25 @@
+﻿using Characters;
+using Units;
+using UnityEngine;
+
+public class CharacterItemExtractor : ICharacterItemExtractor
+{
+    private readonly CityDatabase _cityDatabase;
+
+    public CharacterItemExtractor(CityDatabase cityDatabase)
+    {
+        _cityDatabase = cityDatabase;
+    }
+    public void Extract(IInteractableItem item, CharacterModel characterModel)
+    {
+        if (item == null)
+            return;
+
+        if (characterModel.CharacterCurrentState == ECharacterState.Interact)
+        {
+            Debug.Log(ECharacterState.Interact.ToString());
+            characterModel.View.transform.LookAt(item.Transform);
+            _cityDatabase.Model.Wood += 1f;
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/Controllers/ICharacterItemExtractor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Units;
+using UnityEngine;
+
+public interface ICharacterItemExtractor
+{
+    void Extract(IInteractableItem item, CharacterModel characterModel);
+}
